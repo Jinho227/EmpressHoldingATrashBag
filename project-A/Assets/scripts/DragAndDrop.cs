d4ed81626147924f8bfb2d845cc6cdf2eb7198f4
Jinho227 <sreika227@gmail.com>
2023-04-19 23:02:58 +0900

    feat: create line scene

--- project-A/Assets/scripts/DragAndDrop.cs
+++ project-A/Assets/scripts/DragAndDrop.cs
@@ -7,20 +7,38 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IEndDragHandler
     private CanvasGroup canvasGroup;
     public GameObject Leatheryou;
     public GameObject Leatherme;
+    private bool clear = false;
 
     private void Start()
     {
+
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTransform.anchoredPosition += eventData.delta;
+        if (!clear)
+        {
+            rectTransform.anchoredPosition += eventData.delta;
+        }
+        else
+        {
+
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Leatherme.transform.position = Leatheryou.transform.position;
+        float distance = Vector2.Distance(Leatherme.transform.position, Leatheryou.transform.position);
+        if(distance < 100)
+        {
+            Leatherme.transform.position = Leatheryou.transform.position;
+            clear = true;
+        }
+        else
+        {
+
+        }
     }
 }
